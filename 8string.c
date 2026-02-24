c907a84863c9f5536738b84a2a00ee568009e940
DhaniyaSriAthipathi <athipathidhaniya@gmail.com>
2026-02-24 21:00:34 +0530

    Add files via upload

---
+++ 8string.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    int i=0;
+    int count=0;
+    fgets(str,sizeof(str),stdin);
+    while(str[i]!='\0')
+    {
+        if(str[i]==' ')
+        count++;
+        i++;
+    }
+    printf("%d",count);
+
+    return 0;
+}
