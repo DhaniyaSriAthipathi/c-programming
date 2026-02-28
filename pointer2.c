feac0d922dd0c98f13d7d88f41d784eeb5353a66
DhaniyaSriAthipathi <athipathidhaniya@gmail.com>
2026-02-28 22:03:12 +0530

    Add files via upload

---
+++ pointer2.c
@@ -0,0 +1,25 @@
+#include<Stdio.h>
+int main()
+{
+    int size;
+    scanf("%d",&size);
+    int arr[size];
+    int *p;
+    for(int i=0;i<size;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    p=arr;
+    int max=*p;
+    for(int i=1;i<size;i++)
+    {
+        if(*(p+i)>max)
+        {
+            max=*(p+i);
+        }
+    }
+    printf("%d",max);
+    return 0;
+    
+
+}
